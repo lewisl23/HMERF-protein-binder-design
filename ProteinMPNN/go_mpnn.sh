ef713f8c4d9c68c69b15e2ba6348e13f2808e17b
lewisl23 <lewisliu23@gmail.com>
2026-02-12 15:56:02 +0000

    Add ProteinMPNN script and main execution file

---
+++ ProteinMPNN/go_mpnn.sh
@@ -0,0 +1,23 @@
+cd mpnn_a
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xaa -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_b
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xab -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_c
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xac -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_d
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xad -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_e
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xae -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_f
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xaf -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_g
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xag -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_h
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xah -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_i
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xai -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ../mpnn_j
+nohup ~/dl_binder_design/mpnn_fr/dl_interface_design.py -silent ../xaj -output_intermediates  -checkpoint_path ~/dl_binder_design/mpnn_fr/ProteinMPNN/soluble_model_weights/v_48_020.pt &
+cd ..
+
+
